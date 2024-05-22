ad21f9cdc91d81031dade55d8f9ae7ef54834c2c
James Richardson <Icyfiremario797@gmail.com>
2024-05-22 17:53:22 -0600

    i hate interrupts

--- src/intf/idt.h
+++ src/intf/idt.h
@@ -2,21 +2,3 @@
 
 #include <stddef.h>
 #include <stdint.h>
-
-struct idt_entry
-{
-    uint16_t base_lo;
-    uint16_t sel;
-    uint8_t always0;
-    uint8_t flags;
-    uint16_t base_hi;
-} __attribute__((packed));
-
-struct idt_ptr
-{
-    uint16_t limit;
-    uint32_t base;
-} __attribute__((packed));
-
-static struct idt_entry idt[256];
-static struct idt_ptr idtp;
